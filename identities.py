94aa3833d9d5ad048734f1ad70f8b063e41b9f26
liad-ba <liad.ba93@gmail.com>
2024-03-01 13:54:15 +0200

    add superclass

--- identities.py
+++ identities.py
@@ -1,6 +1,7 @@
 import json
 from dataclasses import dataclass, field, fields, asdict
 from typing import List, Union
+from abc import ABC, abstractmethod
 
 
 @dataclass
@@ -21,26 +22,49 @@ class IdentitySingular:
     contact_id: int
 
 
-class MultipleIdentitiesDataStruct:
+class Cluster(ABC):
+    @abstractmethod
+    def add_item(self, item):
+        pass
+
+    @abstractmethod
+    def __contains__(self, item):
+        pass
+
+    @abstractmethod
+    def __hash__(self):
+        pass
+
+    @abstractmethod
+    def to_dict(self) -> dict:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def from_list(l: List):
+        pass
+
+
+class IdentitiesCluster(Cluster):
 
     def __init__(self):
         self._sets = {}
         for field_name in IdentitySingular.__annotations__.keys():
             self._sets[field_name] = set()
 
-    @classmethod
-    def from_list(cls, lists: List['MultipleIdentitiesDataStruct']):
-        new_instance = cls()
-        for multiple_identities_data_struct in lists:
-            for field_name, field_set in multiple_identities_data_struct._sets.items():
+    @staticmethod
+    def from_list(identities_clusters: List['IdentitiesCluster']):
+        new_instance = IdentitiesCluster()
+        for identities_cluster in identities_clusters:
+            for field_name, field_set in identities_cluster._sets.items():
                 new_instance._sets.setdefault(field_name, set()).update(field_set)
         return new_instance
 
-    def add_identity(self, identity: IdentitySingular):
-        for field_name, field_value in vars(identity).items():
-            self._sets[field_name].add(getattr(identity, field_name))
+    def add_item(self, item):
+        for field_name, field_value in vars(item).items():
+            self._sets[field_name].add(getattr(item, field_name))
 
-    def contains_identity(self, identity: IdentitySingular):
+    def __contains__(self, identity: IdentitySingular):
         return any([
             field_value and field_value in self._sets[field_name]
             for field_name, field_value in vars(identity).items()
@@ -49,17 +73,20 @@ class MultipleIdentitiesDataStruct:
     def __hash__(self):
         return hash(tuple((field_name, frozenset(field_values)) for field_name, field_values in self._sets.items()))
 
-    def to_identities_multiple(self):
-        return IdentitiesMultiple(**{field_name: list(field_set) for field_name, field_set in self._sets.items()})
+    def to_dict(self) -> dict:
+        return asdict(IdentitiesMultiple(**{
+            field_name: list(field_set)
+            for field_name, field_set in self._sets.items()
+        }))
 
 
 def identities_handler(identities):
     identities: List[IdentitySingular] = [IdentitySingular(**identity) for identity in identities]
     clusters = set()
-    for id in identities:
-        relevant_clusters = [cluster for cluster in clusters if cluster.contains_identity(id)]
+    for identity in identities:
+        relevant_clusters = [cluster for cluster in clusters if identity in cluster]
         clusters = clusters.difference(relevant_clusters)
-        new_cluster = MultipleIdentitiesDataStruct.from_list(relevant_clusters)
-        new_cluster.add_identity(id)
+        new_cluster = IdentitiesCluster.from_list(relevant_clusters)
+        new_cluster.add_item(identity)
         clusters.add(new_cluster)
-    return [asdict(cluster.to_identities_multiple()) for cluster in clusters]
+    return [cluster.to_dict() for cluster in clusters]
