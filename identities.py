7e3c44f3d03bef17f0122e14488c36dd906560d5
liad-ba <liad.ba93@gmail.com>
2024-03-01 13:33:47 +0200

    refactor

--- identities.py
+++ identities.py
@@ -21,41 +21,45 @@ class IdentitySingular:
     contact_id: int
 
 
+class MultipleIdentitiesDataStruct:
+
+    def __init__(self):
+        self._sets = {}
+        for field_name in IdentitySingular.__annotations__.keys():
+            self._sets[field_name] = set()
+
+    @classmethod
+    def from_list(cls, lists: List['MultipleIdentitiesDataStruct']):
+        new_instance = cls()
+        for multiple_identities_data_struct in lists:
+            for field_name, field_set in multiple_identities_data_struct._sets.items():
+                new_instance._sets.setdefault(field_name, set()).update(field_set)
+        return new_instance
+
+    def add_identity(self, identity: IdentitySingular):
+        for field_name, field_value in vars(identity).items():
+            self._sets[field_name].add(getattr(identity, field_name))
+
+    def contains_identity(self, identity: IdentitySingular):
+        return any([
+            field_value and field_value in self._sets[field_name]
+            for field_name, field_value in vars(identity).items()
+        ])
+
+    def __hash__(self):
+        return hash(tuple((field_name, frozenset(field_values)) for field_name, field_values in self._sets.items()))
+
+    def to_identities_multiple(self):
+        return IdentitiesMultiple(**{field_name: list(field_set) for field_name, field_set in self._sets.items()})
+
+
 def identities_handler(identities):
     identities: List[IdentitySingular] = [IdentitySingular(**identity) for identity in identities]
-    identity_fields = {f.name: {} for f in fields(IdentitySingular)}
-    for id in identities:
-        for id_field in fields(IdentitySingular):
-            field_name = id_field.name
-            relevant_field_dict = identity_fields[field_name]
-            field_value = getattr(id, field_name)
-            if field_value and field_value not in relevant_field_dict:
-                relevant_field_dict[field_value] = [id]
-            elif field_value:
-                relevant_field_dict[field_value].append(id)
     clusters = set()
-    for k, v in identity_fields.items():
-        for unique_values in v.values():
-            all_relevant_clusters = [cluster for cluster in clusters if any([val in cluster for val in unique_values])]
-            clusters = clusters.difference(all_relevant_clusters)
-            union_set = frozenset(unique_values)
-            for cluster in all_relevant_clusters:
-                union_set = union_set.union(cluster)
-            clusters.add(union_set)
-    all_identities = []
-    for cluster in clusters:
-        account_name = set()
-        account_id =set()
-        deal_id = set()
-        email = set()
-        contact_id = set()
-        for identity in cluster:
-            account_name.add(identity.account_name)
-            account_id.add(identity.account_id)
-            deal_id.add(identity.deal_id)
-            email.add(identity.email)
-            contact_id.add(identity.contact_id)
-        identities_multiple = IdentitiesMultiple(list(account_name),list(account_id),list(deal_id),list(email),list(contact_id))
-
-        all_identities.append(asdict(identities_multiple))
-    return all_identities
+    for id in identities:
+        relevant_clusters = [cluster for cluster in clusters if cluster.contains_identity(id)]
+        clusters = clusters.difference(relevant_clusters)
+        new_cluster = MultipleIdentitiesDataStruct.from_list(relevant_clusters)
+        new_cluster.add_identity(id)
+        clusters.add(new_cluster)
+    return [asdict(cluster.to_identities_multiple()) for cluster in clusters]
